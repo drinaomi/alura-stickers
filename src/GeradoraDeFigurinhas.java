480cb6a3a6f4e9830baaa24fe7dc9e64901f073f
drinaomi <adriana_nakashima@hotmail.com>
2023-03-29 18:07:21 -0300

    terceiro commit

---
+++ src/GeradoraDeFigurinhas.java
@@ -0,0 +1,48 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.font.*;
+
+import javax.imageio.ImageIO;
+
+public class GeradoraDeFigurinhas {
+  
+  public void cria() throws Exception {
+
+    // leitura da imagem
+    BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
+
+    // cria nova imagem com trnsparência e com tamanho novo
+    int largura = imagemOriginal.getWidth();
+    int altura = imagemOriginal.getHeight();
+    int novaAltura = altura + 200;
+    BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+    // copiar a imagem original para nova imagem (em memória)
+    Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+    graphics.drawImage(imagemOriginal, 0, 0, null);
+
+    // configurar a fonte
+    var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+    graphics.setColor(Color.yellow);
+    graphics.setFont(fonte);
+
+    //  escrever uma frase na nova imagem
+    graphics.drawString("TOPZERA", 100, novaAltura - 100);
+
+
+    // escrever a nova imagem em um arquivo
+    ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
+
+  }
+
+  public static void main(String[] args) throws Exception {
+    var geradora = new GeradoraDeFigurinhas();
+    geradora.cria();
+  }
+
+
+}
